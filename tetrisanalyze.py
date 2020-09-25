d8ad3bbf26f49daa73ca6626f3908b35e77d8f2d
sbdevelops <42903115+sbdevelops@users.noreply.github.com>
2020-09-24 22:06:39 -0400

    More tracking parameters and bug fixes

--- tetrisanalyze.py
+++ tetrisanalyze.py
@@ -2,16 +2,53 @@ import argparse
 import cv2
 from collections.abc import Iterable
 
+#
+# DEFAULT VARIABLES
+#
+#
+
+# For the image
+# preloaded_image_name = 'virtualrings.png'
+preloaded_image_name = 1
 window_name = 'Image'
 window_name_gray = "Image (Gray)"
 num_rectangles = 4
 
-image = cv2.imread('virtualrings.png')
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+# For the bounding boxes
+SPACE_DEFAULT = 0
+CHANNEL_DEFAULT = 0
+CENTER_X_DEFAULT = 721
+CENTER_Y_DEFAULT = 878
+WIDTH_DEFAULT = 526
+HEIGHT_DEFAULT = 126
+SPACING_DEFAULT = 0
+SEARCH_SPACE_DEFAULT = 5
+REFRESH_RATE_DEFAULT = 50
 
-shape = image.shape
-img_height = shape[0]
-img_width = shape[1]
+# Names only, not to be changed
+image = None
+gray = None
+img_height = 0
+img_width = 0
+shape = None
+camera = None
+
+
+#
+# USEFUL FUNCTIONS
+#
+#
+def redo_image_stats():
+    global image
+    global gray
+    global img_width
+    global img_height
+    global shape
+    if image is not None:
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        shape = image.shape
+        img_height = shape[0]
+        img_width = shape[1]
 
 
 def constrain(num, minimum, maximum):
@@ -32,7 +69,7 @@ def get_trackbar_vars_as_tuple(*v):
 
 def inverse_color_at_point(point_order_of_xy: tuple):
     # Determine the color we should use for the text, by getting the inverse color at the text's start point
-    color = new_image[point_order_of_xy[0]][point_order_of_xy[1]]
+    color = new_image[point_order_of_xy[1]][point_order_of_xy[0]]
     inverse_color = ~color
     # Convert this inverse color to a tuple and then print it
     inverse_color_as_tuple = \
@@ -42,7 +79,7 @@ def inverse_color_at_point(point_order_of_xy: tuple):
 
 
 def contrain_point_in_image(pt):
-    return constrain(pt[0], 0, shape[0]), constrain(pt[1], 0, shape[1])
+    return constrain(pt[0], 0, shape[1]-1), constrain(pt[1], 0, shape[0]-1)
 
 
 class Rectangle:
@@ -56,11 +93,6 @@ class Rectangle:
         self.bottom_y = self.bottom_right_xy[1]
 
 
-print(shape)
-# cv2.imshow(window_name, image)
-# cv2.imshow(window_name_gray, gray)
-# cv2.waitKey(0)
-
 # Get the valid BGR to ____ color space conversions
 valid_color_spaces = {k: v for k, v in vars(cv2).items() if 'COLOR_BGR2' in k}
 # keys = list(valid_color_spaces.keys())
@@ -73,29 +105,51 @@ keys = (
 )
 print(keys)
 
+#
+# READ THE IMAGE AND DETERMINE PROPERTIES
+#
+#
+if type(preloaded_image_name) is str:
+    using_camera_feed = False
+    image = cv2.imread(preloaded_image_name)
+    redo_image_stats()
+else:
+    using_camera_feed = True
+    camera = cv2.VideoCapture(preloaded_image_name)
+    ret, image = camera.read()
+    redo_image_stats()
+
+print(shape)
+# cv2.imshow(window_name, image)
+# cv2.imshow(window_name_gray, gray)
+# cv2.waitKey(0)
+
 # Create our window
 cv2.namedWindow(window_name, cv2.WINDOW_KEEPRATIO)
 
 # Create trackbars to determine color space and channels
-cv2.createTrackbar('Space', window_name, 0, len(keys), nothing)
-cv2.createTrackbar('Channel', window_name, 0, 1, nothing)
+cv2.createTrackbar('Space', window_name, SPACE_DEFAULT, len(keys), nothing)
+cv2.createTrackbar('Channel', window_name, CHANNEL_DEFAULT, 1, nothing)
 # Create trackbars for the center point
-cv2.createTrackbar('Center X', window_name, 0, img_width, nothing)
-cv2.createTrackbar('Center Y', window_name, 0, img_height, nothing)
+cv2.createTrackbar('Center X', window_name, CENTER_X_DEFAULT, img_width, nothing)
+cv2.createTrackbar('Center Y', window_name, CENTER_Y_DEFAULT, img_height, nothing)
 # Create trackbar for the width, and then the ratio to determine height
-cv2.createTrackbar('Width', window_name, 0, img_width, nothing)
-cv2.createTrackbar('Height', window_name, 0, img_height, nothing)
+cv2.createTrackbar('Width', window_name, WIDTH_DEFAULT, img_width, nothing)
+cv2.createTrackbar('Height', window_name, HEIGHT_DEFAULT, img_height, nothing)
 # Create trackbar for the spacing
-cv2.createTrackbar('Spacing', window_name, 0, int(img_height / 4), nothing)
-# Create trackbar to determine averaging params
-cv2.createTrackbar('Search Space', window_name, 5, 20, nothing)
-
+cv2.createTrackbar('Spacing', window_name, SPACING_DEFAULT, int(img_height / 4), nothing)
+# Create trackbar to determine search params
+cv2.createTrackbar('Search Space', window_name, SEARCH_SPACE_DEFAULT, 20, nothing)
+cv2.createTrackbar('Refresh Rate', window_name, REFRESH_RATE_DEFAULT, 2000, nothing)
 
 while True:
     #
-    # Get correct color space and channels
+    # Refresh image frame, get color space and channels
     #
     #
+    if using_camera_feed and camera is not None:
+        ret, image = camera.read()
+    redo_image_stats()
 
     # Get the desired color space, by obtaining the index from our list
     desired_space_number = cv2.getTrackbarPos('Space', window_name)
@@ -117,15 +171,20 @@ while True:
     cv2.setTrackbarMax('Channel', window_name, num_channels_for_trackbar)
 
     # Get the currently selected channel index
+    channel_index = min(num_channels_for_trackbar, cv2.getTrackbarPos('Channel', window_name))
     # Split the image to one channel if index != 0, otherwise keep image the same
-    channel_index = cv2.getTrackbarPos('Channel', window_name)
     if channel_index != 0:
-        num_channels_in_modified_image = 1
+        # Create a variable for the new image with the single channel
+        new_image_single_channel = new_image[:, :, channel_index - 1]
+
+        # Go through each channel of the image and set it to 0 if it's not our selected channel
         for index in range(0, num_channels_for_trackbar):
             if index != channel_index - 1:
                 new_image[:, :, index] = 0
+    elif num_channels_for_trackbar == 0:
+        new_image_single_channel = new_image
     else:
-        num_channels_in_modified_image = shape[2]
+        new_image_single_channel = None
     # new_image = new_image[:, :, channel_index-1]
 
     # Create an alternate image that we can annotate on when needed
@@ -137,12 +196,13 @@ while True:
     #
 
     # Start off by getting the numbers that we set via the sliders
-    (centerX, centerY, width, height, spacing, search) = \
-        get_trackbar_vars_as_tuple('Center X', 'Center Y', 'Width', 'Height', 'Spacing', 'Search Space')
+    (centerX, centerY, width, height, spacing, search, refresh_rate) = \
+        get_trackbar_vars_as_tuple('Center X', 'Center Y', 'Width', 'Height', 'Spacing', 'Search Space', 'Refresh Rate')
 
     # Put at circle at the center point
-    inverse_color_at_center = inverse_color_at_point((centerX, centerY))
-    cv2.circle(image, (centerX, centerY), 5, inverse_color_at_center, 2)
+    center_point = contrain_point_in_image((centerX, centerY))
+    inverse_color_at_center = inverse_color_at_point(center_point)
+    cv2.circle(annotated, center_point, 5, inverse_color_at_center, 2)
 
     # Find the distance from the center to the edge
     center_x_offset_from_edge = int(width / 2)
@@ -158,6 +218,13 @@ while True:
 
     for n in range(len(rectangles)):
         rectangle = rectangles[n]
+
+        # Place bounding boxes on the annotated image
+        rect_color = inverse_color_at_point(rectangle.top_left_xy)
+        cv2.rectangle(
+            annotated, rectangle.top_left_xy, rectangle.bottom_right_xy,
+            inverse_color_at_point(rectangle.top_left_xy), 5)
+
         # make some reworded variables for the for loop bounds
         row_min = rectangle.top_y
         row_max = rectangle.bottom_y
@@ -165,35 +232,29 @@ while True:
         col_max = rectangle.right_x
 
         # If there is a channel in the image, and the search rectangle has an area greater than 0
-        if num_channels_in_modified_image > 0 and (row_max - row_min) * (col_max - col_min) != 0:
+        if new_image_single_channel is not None and (row_max - row_min) * (col_max - col_min) != 0:
 
             # Do the numerical analysis
 
-            num_points_searched = 0
+            # num_points_searched = 0
             points_sum = 0
             points = list()
-            for row_inc in range(row_min, row_min + height, search+1):
-                for col_inc in range(col_min, col_min + height, search+1):
-                    point_val = new_image[row_inc][col_inc]
-                    points.append(new_image[row_inc][col_inc])
+            for row_inc in range(row_min, row_max, search + 1):
+                for col_inc in range(col_min, col_max, search + 1):
+                    point_val = new_image_single_channel[row_inc][col_inc]
+                    points.append(new_image_single_channel[row_inc][col_inc])
                     points_sum += point_val
-            average = points_sum / num_points_searched
+            average = points_sum / len(points)
 
             stddev_sum = 0
             for point in points:
                 stddev_sum = pow((point - average), 2)
-            stddev = pow(stddev_sum / (num_points_searched - 1), 0.5)
+            stddev = pow(stddev_sum / (len(points) - 1), 0.5)
 
-            # Place bounding boxes on the annotated image
-            rect_color = inverse_color_at_point(rectangle.top_left_xy)
-            cv2.rectangle(
-                annotated, rectangle.top_left_xy, rectangle.bottom_right_xy,
-                inverse_color_at_point(rectangle.top_left_xy), 5)
-
-            rect_text_start_point = (5, img_height - 20 - (10 * n))
+            rect_text_start_point = (5, img_height - (30 * (n + 1)))
             text_color = inverse_color_at_point(rect_text_start_point)
             # Put the text on our image
-            cv2.putText(annotated, "Rect %d: avg%.2f stdev%.2f pts%d" % (n, average, stddev, num_points_searched),
+            cv2.putText(annotated, "Rect %d: avg%.2f stdev%.2f pts%d" % (n, average, stddev, len(points)),
                         rect_text_start_point, cv2.FONT_HERSHEY_SIMPLEX, 0.5, text_color, 2)
 
     #
@@ -209,4 +270,4 @@ while True:
 
     # Actually show the image, and wait for a while
     cv2.imshow(window_name, annotated)
-    cv2.waitKey(50)
+    cv2.waitKey(refresh_rate)
